4c3202d1460df37cddc94c8c8766e293f4935f48
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-11 23:57:20 +0300

    add directory common

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import style from './Main.module.css'
-
+import styleContainer from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return <div className={style.mainBlock}>
 
-        <div className={style.container}>
+        <div className={styleContainer.container}>
 
             <div className={style.greeting}>
                 <span> Hi There </span>
