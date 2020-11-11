0fe4b3e954765ee1fd37f85776273a8f56a87e06
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-11 23:53:35 +0300

    style Main.tsx

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -5,5 +5,17 @@ import style from './Main.module.css'
 export const Main = () => {
     return <div className={style.mainBlock}>
 
+        <div className={style.container}>
+
+            <div className={style.greeting}>
+                <span> Hi There </span>
+                <h1> I am Filimonov Kostya </h1>
+                <p>React developer</p>
+            </div>
+            <div className={style.photo}>
+
+            </div>
+
+        </div>
     </div>
 }
