ab5d4428a9dd37a75756f648892c5a3a0ba1e6e8
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-16 21:58:55 +0300

    skill,skills

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import {Header} from "./Header/Header";
 import {Main} from "./Main/Main";
+import {Skills} from "./Skills/Skills";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Main/>
+            <Skills/>
         </div>
     );
 }
 
+
+// 1: 28 : 10 про 3 блок где скиллы
 export default App;
---
+++ src/Skills/Skil/Skill.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from './Skil.module.css'
+
+export const Skill = () => {
+    return <div>
+
+    </div>
+}
