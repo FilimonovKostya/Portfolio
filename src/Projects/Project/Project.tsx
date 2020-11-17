c1466c4c6a3cb53df13ba2d356a223f03afc6fe4
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-17 18:28:00 +0300

    projects

---
+++ src/Projects/Project/Project.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "../Projects.module.css";
+
+type ProjectPropsType = {
+    backgroundImg:{}
+}
+
+export const Project = (props:ProjectPropsType) => {
+    return <div className={style.containerProject}>
+        <div className={style.ownProjects}>
+            <div style={props.backgroundImg} className={style.backgroundImg}>
+                <button className={style.buttonWatch}>Watch</button>
+            </div>
+            <h4>Name Project</h4>
+            <p>Description</p>
+        </div>
+    </div>
+}
--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from './Projects.module.css'
 import background from '../assets/img/background.jpeg'
+import {Project} from "./Project/Project";
 
 export const Projects = () => {
 
@@ -13,26 +14,13 @@ export const Projects = () => {
 
     return <div className={style.blockProjects}>
         <div className={style.aboutProjects}>
-            <div className={style.projectHeader}><h3>My Projects</h3></div>
 
-            <div className={style.containerProject}>
-                <div className={style.ownProjects}>
-                    <div style={backgroundImg} className={style.backgroundImg}>
-                        <button className={style.buttonWatch}>Watch</button>
-                    </div>
-                    <h4>Name Project</h4>
-                    <p>Description</p>
-                </div>
-
-
-                <div className={style.ownProjects}>
-                    <div style={backgroundImg} className={style.backgroundImg}>
-                        <button className={style.buttonWatch}>Watch</button>
-                    </div>
-                    <h4>Name Project</h4>
-                    <p>Description</p>
-                </div>
+            <div className={style.projectHeader}>
+                <h3>My Projects</h3>
             </div>
+
+            <Project backgroundImg={backgroundImg}/>
+            <Project backgroundImg={backgroundImg}/>
 
         </div>
     </div>
