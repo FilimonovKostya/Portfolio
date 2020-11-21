c99e0c319373f6c93ed553673c02bc5225c4912d
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-21 20:53:01 +0300

    Удалил паппку  с этим лять контенейром

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import style from './Main.module.css'
-import styleContainer from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return <div className={style.mainBlock}>
 
-        <div className={styleContainer.container}>
+        <div >
 
             <div className={style.greeting}>
                 <span> Hi There </span>
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import style from './Skils.module.css'
-import styleContainer from '../common/styles/Container.module.css'
 import {Skill} from "./Skil/Skill";
 
 
@@ -8,7 +7,7 @@ export const Skills = () => {
     const description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cumque deserunt ea harum labore laborum magnam maiores, natus ratione, recusandae'
 
     return <div className={style.skillsBlock}>
-        <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+        <div className={`${style.skillsContainer}`}>
             <h2 className={style.title}>Skills</h2>
             <div className={style.skills}>
                 <Skill title={'HTML'} description={description}/>
