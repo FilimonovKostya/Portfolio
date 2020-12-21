8b3e249e326db7d5c5be4dfcfec9d508494047d4
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-12-21 22:12:35 +0300

    украл классную кнопочоку ,потом разукрашу

--- src/Job/HireBlock.tsx
+++ src/Job/HireBlock.tsx
@@ -4,8 +4,8 @@ import style from "./HireBlock.module.css";
 export const HireBlock = () => {
   return <section className={style.wrapper}>
       <div className={style.hireBLock}>
-        <h4>Considering options for remote work</h4>
-        <button className={style.button}>Click Me</button>
+        <h4>Download CV</h4>
+        <button className={style.button}>Click</button>
       </div>
     </section>
   
