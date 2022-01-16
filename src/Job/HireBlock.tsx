c1fb8cdd69d112d7c0d9c1922fdd9ecb9054a7aa
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-01-16 12:20:13 +0300

    was added link for cv

--- src/Job/HireBlock.tsx
+++ src/Job/HireBlock.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import style from "./HireBlock.module.scss";
 
 export const HireBlock = () => {
-  return <section className={style.wrapper}>
-      <div className={style.hireBLock}>
-        <h4>Download CV</h4>
-    <a href={'https://drive.google.com/file/d/1x-lkwMHmt375FT4lSoAdqmjQADJb9KP0/view?usp=sharing'} target={'blank'} className={style.button} download> Click </a>
-      </div>
+    return <section className={style.wrapper}>
+        <div className={style.hireBLock}>
+            <h4>Download CV</h4>
+            <a href={'https://drive.google.com/file/d/1fEeDIY3zp4dhDkIaTcX9HFpVoEkkPqh7/view?usp=sharing'} target={'blank'} className={style.button}
+               download> Click </a>
+        </div>
     </section>
-  
+
 };
